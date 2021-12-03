e9d706fdd2f48b501177b271a92667a654d8d02d
Filip Štolfa <phillipstolfa@gmail.com>
2021-12-03 01:14:48 +0100

    Day 2

---
+++ 2b.hs
@@ -0,0 +1,53 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+import AOC
+import Data.Char
+import Data.Tuple.HT
+
+
+data Dir = Forward | Down | Up deriving (Show, Bounded, Eq, Enum)
+
+main :: IO ()
+main = interactl $ f . rights . map (parse p)
+
+chari :: Char -> Parser Char
+chari c = oneOf [toLower c, toUpper c]
+
+stringi :: String -> Parser String
+stringi = mapM chari
+
+enump :: forall b. (Enum b, Bounded b, Show b) => Parser b
+enump = choice $ map sr [minBound :: b..maxBound :: b]
+  where
+    sr :: (Show b) => b -> Parser b
+    sr x = try $ stringi (show x) >> return x
+
+
+rights (Right x:xs) = x: rights xs
+rights (_:xs) = rights xs
+rights [] = []
+
+
+p :: Parser (Dir, Int)
+p = do
+  dirStr <- enump
+  char ' '
+  amount <- many1 digit
+  return (dirStr, read amount)
+
+f :: [(Dir, Int)] -> Int
+f xs = uncurry (*) $ (\(x,y,z) -> (x,y)) $ foldl c (0,0,0) xs
+  where
+    c :: (Int, Int, Int) -> (Dir, Int) -> (Int, Int, Int)
+    c acc coord
+      | dir == Forward = (h+a, d+(aim*a), aim)
+      | dir == Down = (h, d, aim+a)
+      | dir == Up = (h, d, aim-a)
+      | otherwise = acc
+      where
+        dir = fst coord
+        a = snd coord
+        h = fst3 acc
+        d = snd3 acc
+        aim = thd3 acc
+
+
