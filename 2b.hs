7edebb6eb4e771e9b1f53d18794e0a3c8785ec0a
Filip Štolfa <phillipstolfa@gmail.com>
2021-12-12 11:47:58 +0100

    Move parsers to AOC; Add helper functions to AOC

--- 2b.hs
+++ 2b.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE ScopedTypeVariables #-}
 import AOC hiding(Down)
 import Data.Char
 import Data.Tuple.HT
@@ -8,23 +7,6 @@ data Dir = Forward | Down | Up deriving (Show, Bounded, Eq, Enum)
 
 main :: IO ()
 main = interactl $ f . rights . map (parse p)
-
-chari :: Char -> Parser Char
-chari c = oneOf [toLower c, toUpper c]
-
-stringi :: String -> Parser String
-stringi = mapM chari
-
-enump :: forall b. (Enum b, Bounded b, Show b) => Parser b
-enump = choice $ map sr [minBound :: b..maxBound :: b]
-  where
-    sr :: (Show b) => b -> Parser b
-    sr x = try $ stringi (show x) >> return x
-
-
-rights (Right x:xs) = x: rights xs
-rights (_:xs) = rights xs
-rights [] = []
 
 
 p :: Parser (Dir, Int)
--- AOC.hs
+++ AOC.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 module AOC(module Prelude, module AOC, module Text.Parsec) where
 
 import Prelude hiding(interact)
@@ -6,13 +7,43 @@ import qualified Prelude
 import Text.Parsec hiding(count, parse)
 import qualified Text.Parsec as Parsec
 
+import qualified Data.List.Split as Split
+
+import Data.Char
+
+interactw :: Show a => ([String] -> a) -> IO()
+interactw f = Prelude.interact $ (++"\n") . show . f . words
+
+interacts :: Show a => String -> ([String] -> a) -> IO()
+interacts p f = Prelude.interact $ (++"\n") . show . f . Split.splitOn p
+
 interactl :: Show a => ([String] -> a) -> IO ()
 interactl f = Prelude.interact $ (++"\n") . show . f . lines
+
+
+rights (Right x:xs) = x: rights xs
+rights (_:xs) = rights xs
+rights [] = []
 
 type Parser = Parsec String ()
 
 parse :: Parser a -> String -> Either ParseError a
 parse p = Parsec.parse p ""
+
+chari :: Char -> Parser Char
+chari c = oneOf [toLower c, toUpper c]
+
+stringi :: String -> Parser String
+stringi = mapM chari
+
+enump :: forall b. (Enum b, Bounded b, Show b) => Parser b
+enump = choice $ map sr [minBound :: b..maxBound :: b]
+  where
+    sr :: (Show b) => b -> Parser b
+    sr x = try $ stringi (show x) >> return x
+
+fromDigits :: [Int] -> Int
+fromDigits = foldl (\y x -> x + y*10) 0
 
 toDec :: [Bool] -> Int
 toDec = foldl (\y x -> fromEnum x + y*2) 0
@@ -22,3 +53,6 @@ toDec' = foldl (\y x -> fromEnum x + y*2) 0 . map (=='1')
 
 multPair :: (Int, Int) -> Int
 multPair = uncurry (*)
+
+ints :: [String] -> [Int]
+ints = map read
